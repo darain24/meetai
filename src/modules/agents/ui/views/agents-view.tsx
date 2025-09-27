05b829b54c6dcce2b5bce6e3c254d6a065597039
darain24 <darainqamar10@gmail.com>
2025-09-27 23:28:02 +0530

    data-agents-table

--- src/modules/agents/ui/views/agents-view.tsx
+++ src/modules/agents/ui/views/agents-view.tsx
@@ -1,34 +1,43 @@
-'use client'
+"use client";
 
-import { ErrorState } from "@/components/error-state"
-import { LoadingState } from "@/components/loading-state"
-import { Button } from "@/components/ui/button"
-import { trpc } from "@/trpc/client"
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
+import { Button } from "@/components/ui/button";
+import { trpc } from "@/trpc/client";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
-    const [data] = trpc.agents.getMany.useSuspenseQuery()
+  const [data] = trpc.agents.getMany.useSuspenseQuery();
 
-    return(
-        <div>
-            {JSON.stringify(data,null,2)}
-        </div>
-    )
-}
+  return (
+    <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+      <DataTable data={data} columns={columns} />
+      {data.length === 0 && (
+        <EmptyState
+          title="Create your first agent"
+          description="Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call"
+        />
+      )}
+    </div>
+  );
+};
 
 export const AgentsViewLoading = () => {
-    return (
-        <LoadingState 
-            title="Loading Agents"
-            description="This may take a few seconds"
-        />
-    )
-}
+  return (
+    <LoadingState
+      title="Loading Agents"
+      description="This may take a few seconds"
+    />
+  );
+};
 
 export const AgentsViewError = () => {
-    return (
-         <ErrorState
-            title = 'Error Loading Agents'
-            description = 'Something went wrong'
-        />
-    )
-}
+  return (
+    <ErrorState
+      title="Error Loading Agents"
+      description="Something went wrong"
+    />
+  );
+};
