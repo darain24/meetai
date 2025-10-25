46a17cca0833b354546cfa49efa21244e931a910
darain24 <darainqamar10@gmail.com>
2025-10-25 10:25:52 +0530

    meetings-data-table

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -3,7 +3,7 @@ import { agents, meetings } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 import z from "zod";
-import { and, desc, eq, getTableColumns, ilike, count } from "drizzle-orm";
+import { and, desc, eq, getTableColumns, ilike, count, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
 
@@ -70,8 +70,11 @@ export const meetingsRouter = createTRPCRouter({
        const data = await db
          .select({
            ...getTableColumns(meetings),
+           agent: agents,
+           duration: sql<number>`EXTRACT(EPOCH FROM (ended_at - started_at))`.as("duration")
          })
          .from(meetings)
+         .innerJoin(agents, eq(meetings.agentId, agents.id))
          .where(
              and(
                  eq(meetings.userId, ctx.auth.user.id),
@@ -85,6 +88,7 @@ export const meetingsRouter = createTRPCRouter({
          const [total] = await db
          .select({count : count()})
          .from(meetings)
+         .innerJoin(agents, eq(meetings.agentId, agents.id))
          .where(
              and(
                  eq(meetings.userId, ctx.auth.user.id),
---
+++ src/modules/meetings/ui/components/columns.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { ColumnDef } from "@tanstack/react-table";
+import { MeetingGetMany } from "../../types";
+import { GeneratedAvatar } from "@/components/generated-avatar";
+import { 
+  CircleCheckIcon,
+  CircleXIcon,
+  ClockArrowUpIcon,
+  ClockFadingIcon,
+  CornerDownRightIcon,
+  LoaderIcon,
+  VideoIcon
+ } from "lucide-react";
+import {cn} from '@/lib/utils' 
+import { Badge } from "@/components/ui/badge";
+import  humanizeDuration  from "humanize-duration";
+import { format } from "date-fns";
+
+
+function formatDuration(seconds: number){
+  return humanizeDuration(seconds * 1000, {
+    largest: 1,
+    round: true,
+    units: ['h', 'm', 's'],
+  });
+}
+
+const statusIconMap = {
+  upcoming: ClockArrowUpIcon,
+  active: LoaderIcon,
+  completed: CircleCheckIcon,
+  cancelled: CircleXIcon,
+  processing: ClockFadingIcon,
+}
+
+const statusColorMap ={
+  upcoming: 'text-yellow-800 bg-yellow-500/20 border-yellow-800/5',
+  active: 'text-blue-800 bg-blue-500/20 border-blue-800/5',
+  completed: 'text-emerald-800 bg-emerald-500/20 border-emerald-800/5',
+  cancelled: 'text-rose-800 bg-rose-500/20 border-rose-800/5',
+  processing: 'text-gray-800 bg-gray-500/20 border-gray-800/5',
+}
+
+export const columns: ColumnDef<MeetingGetMany[number]>[] = [
+  {
+    accessorKey: "name",
+    header: "Meeting Name",
+    cell: ({ row }) => (
+      <div className="flex flex-col gap-y-1">
+        <span className="font-semibold capitalize">{row.original.name}</span>
+        <div className="flex items-center gap-x-2">
+          <div className="flex items-center gap-x-1"></div>
+            <CornerDownRightIcon className="size-3 text-muted-foreground" />
+            <span className="text-sm text-muted-foreground max-w-[200px] truncate">
+              {row.original.agent.name}
+            </span>
+        </div>
+        <GeneratedAvatar 
+          seed={row.original.agent.name}
+          variant="botttsNeutral"
+          className="size-4"
+        />
+        <span className="text-sm text-muted-foreground">
+          {row.original.startedAt ? format(row.original.startedAt, 'MMM d') : ''}
+        </span>
+      </div>
+    ),
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({row}) => {
+      const Icon = statusIconMap[row.original.status as keyof typeof statusIconMap];
+      return (
+        <Badge
+          variant="outline"
+          className={cn("capitalize flex items-center gap-x-2 [&>svg]:size-4", statusColorMap[row.original.status as keyof typeof statusColorMap])}
+        >
+          <Icon className={cn(
+            row.original.status === 'processing' && 'animate-spin'
+          )}/>
+          {row.original.status}
+        </Badge>
+      );
+    },
+  },
+  {
+    accessorKey: "duration",
+    header: "Duration",
+    cell: ({row}) => (
+      <Badge 
+        variant="outline"
+        className="capitalize flex items-center gap-x-2 [&>svg]:size-4"
+      >
+        <ClockFadingIcon className='text-blue-700'/>
+        {row.original.duration ? formatDuration(row.original.duration) : 'No duration'}
+      </Badge>
+    ),
+  }
+];
