8866828615735b1a8f52241148384899687f7ff9
darain24 <darainqamar10@gmail.com>
2025-12-25 16:00:44 +0530

    Make channels page dynamic to fix useSearchParams prerender error

--- src/app/(dashboard)/channels/page.tsx
+++ src/app/(dashboard)/channels/page.tsx
@@ -1,5 +1,7 @@
 import { ChannelsView } from "@/modules/channels/ui/views/channels-view";
 
+export const dynamic = 'force-dynamic'
+
 export default function ChannelsPage() {
   return <ChannelsView />;
 }
